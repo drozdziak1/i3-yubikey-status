6e49e214b072f34cdbea5da5e2571c9cea34592e
Stan Drozd <drozdziak1@gmail.com>
2021-05-18 01:25:20 +0200

    Add a barebones program to list card readers

--- flake.nix
+++ flake.nix
@@ -1,11 +1,19 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+      {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        packages."${system}".hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+        defaultPackage."${system}" = self.packages."${system}".hello;
+        devShell."${system}" = pkgs.mkShell {
+          buildInputs = with pkgs; [pkg-config libusb pcsclite];
+        };
 
-  };
+      };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use pcsc::{Context, Scope};
+pub type ErrBox = Box<dyn std::error::Error>;
+
+fn main() -> Result<(), ErrBox> {
+    let ctx = Context::establish(Scope::User)?;
+
+    let names = ctx.list_readers_owned()?;
+
+    println!("names: {:?}", names);
+
+    Ok(())
 }
